6f59b7903f13ecde625f1523041e04da4a808b83
mike <https://github.com/MikhailSokol>
2022-04-04 17:01:38 +0300

    configs_2

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/my-app/index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.set('port', port);
 
 const server = http.createServer(app);
